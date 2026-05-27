63b73aedd64c627fe0f2533e3dae77bdd092587d
Yellowblacklord <bluebookdevil@gmail.com>
2026-05-27 12:31:21 +0800

    Build Fix: Explicitly bind externalNativeBuild CMake and add loadLibrary to Engine

--- app/src/main/java/com/monitor/game/Engine.java
+++ app/src/main/java/com/monitor/game/Engine.java
@@ -3,11 +3,12 @@ package com.monitor.game;
 import android.graphics.Bitmap;
 
 public class Engine {
+    // 关键：必须在类加载时，第一时间强行把 C++ 库灌入内存
     static {
-        try { System.loadLibrary("native-lib"); }
-        catch (UnsatisfiedLinkError e) { android.util.Log.e("GameEngine", "❌ 核心驱动加载失败！"); }
+        System.loadLibrary("native-lib");
     }
+
+    // 底层 C++ 协同接口定义
     public native boolean initDetector(String param320, String bin320, String param416, String bin416);
-    // 修改返回类型为 float[]
     public native float[] detectScreen(Bitmap bitmap);
 }
